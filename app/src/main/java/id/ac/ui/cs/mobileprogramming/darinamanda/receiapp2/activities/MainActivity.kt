3527c10b43fbe389ebe15563cff55bccccf74504
Darin Amanda Zakiya <darinamandaz@gmail.com>
2020-11-24 21:30:28 +0700

    done

---
+++ app/src/main/java/id/ac/ui/cs/mobileprogramming/darinamanda/receiapp2/activities/MainActivity.kt
@@ -0,0 +1,21 @@
+package id.ac.ui.cs.mobileprogramming.darinamanda.receiapp2.activities
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.ui.AppBarConfiguration
+import id.ac.ui.cs.mobileprogramming.darinamanda.receiapp2.R
+import androidx.lifecycle.ViewModelProviders
+import id.ac.ui.cs.mobileprogramming.darinamanda.receiapp2.viewmodel.RecipeViewModel
+
+
+class MainActivity : AppCompatActivity(){
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_homepage)
+
+    }
+
+
+}
